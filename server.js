77ce03ef040ed4ff3a157ba83d77cc0c9f0e49f8
Timothy Swengros <timswengros@Timothys-MacBook-Pro.local>
2018-05-11 23:14:04 -0400

    added psuedcode in survey.html

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
-require(path.join(__dirname, './app/routing/apiRoutes'))(app);
-require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+require(path.join(__dirname, './app/routing/apiRoutes.js'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes.js'))(app);
 
 app.listen(PORT, function() {
-  console.log('Friend Finder app is listening on PORT: ' + PORT);
+  console.log('Friend Finder app is listening on PORT: 8889 ' + PORT);
 });
